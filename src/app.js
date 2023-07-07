d83087d4e7db5c57cf8589a99eb174a17917b769
justino2m <justin.viola@o2m.systems>
2023-07-07 10:19:22 -0400

    test env file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { FORWidget } from "./for-widget.js";
 import CONSTANT from "./constant.js";
 
+console.log(">>process.env.SIGNUP_URL", process.env.SIGNUP_URL);
 const forWidget = new FORWidget({
   position: CONSTANT.POSITION,
   vertical: CONSTANT.VERTICAL,
